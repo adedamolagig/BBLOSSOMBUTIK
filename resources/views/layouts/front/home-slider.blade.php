a619083ef725e4029b00207015f4d5355a0dbff9
adedamolagig <adedamolagig@gmail.com>
2018-08-11 00:25:24 +0100

    first commit

--- resources/views/layouts/front/home-slider.blade.php
+++ resources/views/layouts/front/home-slider.blade.php
@@ -3,10 +3,11 @@
         <div class="row">
             <div class="col-md-8">
                 <div class="hero-content">
-                    <h1 class="hero-title">{{config('app.name')}} Holiday <br> Collection</h1>
+                    <!-- inserting store name at landing page -->
+                    <h1 class="hero-title"><!-- {{config('app.name')}} --> B-Blossom <br> Butik</h1>
                     <p class="hero-text">Download this amazing e-commerce web app for <strong class="text-success">FREE!</strong></p>
-                    <a class="btn btn-success" href="https://github.com/jsdecena/laracom" target="_blank" role="button">DOWNLOAD <i class="fa fa-cloud-download"></i></a>
-                    <a class="btn btn-danger" href="https://m.do.co/c/bce94237de96" target="_blank" role="button">Build on DigitalOcean <i class="fa fa-paper-plane"></i></a>
+                    <a class="btn btn-success" href="#!" target="_blank" role="button">Selling Fast <i class="fa fa-cloud-download"></i></a>
+                    <a class="btn btn-danger" href="#!" target="_blank" role="button">Top Brands <i class="fa fa-paper-plane"></i></a>
                 </div>
             </div>
             <div class="col-md-4"></div>
