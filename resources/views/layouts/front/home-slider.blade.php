11ace49ef7a8056d08967b848fa777b2d0d0c905
adedamolagig <adedamolagig@gmail.com>
2018-08-11 16:13:11 +0100

    made some branding changes and effected npm run watch on the server

--- resources/views/layouts/front/home-slider.blade.php
+++ resources/views/layouts/front/home-slider.blade.php
@@ -5,7 +5,7 @@
                 <div class="hero-content">
                     <!-- inserting store name at landing page -->
                     <h1 class="hero-title"><!-- {{config('app.name')}} --> B-Blossom <br> Butik</h1>
-                    <p class="hero-text">Download this amazing e-commerce web app for <strong class="text-success">FREE!</strong></p>
+                    <p class="hero-text">Products are selling out <strong class="text-success">FAST!</strong></p>
                     <a class="btn btn-success" href="#!" target="_blank" role="button">Selling Fast <i class="fa fa-cloud-download"></i></a>
                     <a class="btn btn-danger" href="#!" target="_blank" role="button">Top Brands <i class="fa fa-paper-plane"></i></a>
                 </div>
