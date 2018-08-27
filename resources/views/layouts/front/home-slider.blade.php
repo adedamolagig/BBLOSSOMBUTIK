7030b7b8983f0c8a5509a600e55f06f966a4ebe2
adedamolagig <adedamolagig@gmail.com>
2018-08-27 07:52:45 -0400

    made some changes to front_landing page

--- resources/views/layouts/front/home-slider.blade.php
+++ resources/views/layouts/front/home-slider.blade.php
@@ -4,10 +4,10 @@
             <div class="col-md-8">
                 <div class="hero-content">
                     <!-- inserting store name at landing page -->
-                    <h1 class="hero-title"><!-- {{config('app.name')}} --> B-Blossom <br> Butik</h1>
-                    <p class="hero-text">Products are selling out <strong class="text-success">FAST!</strong></p>
+                    <h1 class="hero-title"><!-- {{config('app.name')}} --> </h1>
+                   <!--  <p class="hero-text">Products are selling out <strong class="text-success">FAST!</strong></p>
                     <a class="btn btn-success" href="#!" target="_blank" role="button">Selling Fast <i class="fa fa-cloud-download"></i></a>
-                    <a class="btn btn-danger" href="#!" target="_blank" role="button">Top Brands <i class="fa fa-paper-plane"></i></a>
+                    <a class="btn btn-danger" href="#!" target="_blank" role="button">Top Brands <i class="fa fa-paper-plane"></i></a> -->
                 </div>
             </div>
             <div class="col-md-4"></div>
