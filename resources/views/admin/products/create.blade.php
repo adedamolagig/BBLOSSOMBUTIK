b8487e93bf816841a89dcc2f30ba3419ee72ed83
adedamolagig <adedamolagig@gmail.com>
2018-10-03 18:10:12 +0100

    changed

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -38,7 +38,7 @@
                         <div class="form-group">
                             <label for="price">Price <span class="text-danger">*</span></label>
                             <div class="input-group">
-                                <span class="input-group-addon">PHP</span>
+                                <span class="input-group-addon">Naira</span>
                                 <input type="text" name="price" id="price" placeholder="Price" class="form-control" value="{{ old('price') }}">
                             </div>
                         </div>
